82c21f62ea126e7ddd5180ad60d00f2698698eae
Jurģis Krastiņš <jurgis.krastins@gmail.com>
2020-12-20 13:54:33 +0200

    add conversion to from note to midi pitch

--- src/main/scala/model/Note.scala
+++ src/main/scala/model/Note.scala
@@ -1,5 +1,6 @@
 package model
 
+
 object NoteName extends Enumeration {
   type NoteName = Value
   // no way to distinguish sharps from flats, because we don't know the scale
@@ -8,10 +9,14 @@ object NoteName extends Enumeration {
   def fromInt(value: Int): Option[NoteName] = values.find(_.id == value)
 }
 
+import model.NoteName.NoteName
 
+case class Note(name: NoteName, octave: Int, length: NoteLength = Eight) {
+  def toMidiPitch: Either[String, Int] = {
+    val pitch = name.id + 12 * octave + 24
+    if ((0 to 127).contains(pitch)) Right(pitch) else Left(f"Pitch out of range: $pitch")
+  }
 }
-
-case class Note(name: NoteName, octave: Int)
 
 object Note {
   // MIDI note 0 = C-2; MIDI note 24 = C0, aligned with Ableton Live
--- src/test/scala/model/NoteSpec.scala
+++ src/test/scala/model/NoteSpec.scala
@@ -2,11 +2,10 @@ package model
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import model.NoteName._
 
 class NoteSpec extends AnyFlatSpec with Matchers {
-
-  import NoteName._
-
+  
   "note" should "be parsed from MIDI pitch" in {
     Note.from(0) should contain (Note(name = C, octave = -2))
     Note.from(1) should contain (Note(name = CSharp, octave = -2))
@@ -19,5 +18,32 @@ class NoteSpec extends AnyFlatSpec with Matchers {
     Note.from(36) shouldBe Some(Note(name = C, octave = 1))
     Note.from(42) shouldBe Some(Note(name = FSharp, octave = 1))
     Note.from(48) shouldBe Some(Note(name = C, octave = 2))
+  }
+
+  "toMidiPitch" should "convert list of grouped notes to list of internal note representation" in {
+    Note(C, -2).toMidiPitch shouldBe Right(0)
+    Note(C, -1).toMidiPitch shouldBe Right(12)
+    Note(A, -1).toMidiPitch shouldBe Right(21)
+    Note(C, 0).toMidiPitch shouldBe Right(24)
+    Note(C, 1).toMidiPitch shouldBe Right(36)
+    Note(CSharp, 1).toMidiPitch shouldBe Right(37)
+    Note(D, 1).toMidiPitch shouldBe Right(38)
+    Note(DSharp, 1).toMidiPitch shouldBe Right(39)
+    Note(E, 1).toMidiPitch shouldBe Right(40)
+    Note(F, 1).toMidiPitch shouldBe Right(41)
+    Note(FSharp, 1).toMidiPitch shouldBe Right(42)
+    Note(G, 1).toMidiPitch shouldBe Right(43)
+    Note(GSharp, 1).toMidiPitch shouldBe Right(44)
+    Note(A, 1).toMidiPitch shouldBe Right(45)
+    Note(ASharp, 1).toMidiPitch shouldBe Right(46)
+    Note(B, 1).toMidiPitch shouldBe Right(47)
+    Note(C, 3).toMidiPitch shouldBe Right(60)
+    Note(G, 8).toMidiPitch shouldBe Right(127)
+  }
+
+  it should "only work within the 8 bits of MIDI pitch" in {
+    import NoteName._
+    Note(B, -3).toMidiPitch shouldBe Left("Pitch out of range: -1")
+    Note(GSharp, 8).toMidiPitch shouldBe Left("Pitch out of range: 128")
   }
 }
