c33eefd0285870b3765db8ed4c3c4b3acf194028
Jurģis Krastiņš <jurgis.krastins@gmail.com>
2020-12-20 15:42:33 +0200

    write list of notes and rests to a MIDI file

--- src/main/scala/midi/Midi.scala
+++ src/main/scala/midi/Midi.scala
@@ -1,13 +1,19 @@
 package midi
 
-import de.sciss.midi.{Event, NoteOff, NoteOn}
-import model.Note
+import de.sciss.midi.{Event, NoteOff, NoteOn, Sequence, TickRate, Track}
+import model.NoteLength.NoteLength
+import model.{Melody, Note, NoteLength, NoteOrRest, Rest}
 
 import java.nio.file.Paths
 import scala.annotation.tailrec
 import scala.util.Try
 
 object Midi {
+  val Bpm = 120
+  val Velocity = 100
+
+  // this is supposed to be a constant in the context of this this class, and an implicit for de.sciss.midi.Track
+  implicit val tickRate: TickRate = TickRate.apply(NoteLength.TicksPerQuarterNote * Bpm / 60)
 
   def filterNoteEvents(events: Seq[Event]): Seq[Event] =
     events.filter(e => e.message match {
@@ -82,4 +88,44 @@ object Midi {
       sequence <- toNotesWithRests(grouped)
     } yield Melody(sequence)
   }
+
+  def midiNoteLength(noteLength: NoteLength): Long = {
+    val ticksPerSecond = tickRate.value
+    val ticksPerQuarterNote = ticksPerSecond * 60 / Bpm
+    (noteLength.id.toDouble / NoteLength.TicksPerQuarterNote * ticksPerQuarterNote).toLong
+  }
+
+  def midiEventsFrom(notes: List[NoteOrRest]): Either[String, List[Event]] = {
+    import cats.implicits._
+    val acc = Either.right[String, (List[Event], Long)]((List.empty[Event], 0))
+    notes.foldLeft(acc)((res: Either[String, (List[Event], Long)], noteOrRest: NoteOrRest) => {
+      res.flatMap { case (events: List[Event], restTick: Long) =>
+        val lastTick = List[Long](events.headOption.map(_.tick).getOrElse(0), restTick).max
+        noteOrRest match {
+          case note@Note(_, _, _) =>
+            val ticks = lastTick + midiNoteLength(note.length)
+            note.toMidiPitch.map(pitch =>
+              (Event(ticks, NoteOff(0, pitch, 0)) :: Event(lastTick, NoteOn(0, pitch, Velocity)) :: events, ticks))
+          case rest@Rest(_) =>
+            val delta = lastTick + midiNoteLength(rest.length)
+            Right(events, delta)
+
+        }
+      }
+    }).map { case (events, _) => events.reverse }
+  }
+
+  def createMidiSequence(notes: List[NoteOrRest]): Either[String, Sequence] = {
+    import cats.implicits._
+    for {
+      eventList <- midiEventsFrom(notes)
+      events = eventList.toIndexedSeq
+      lastTick = events.map(_.tick).max
+      track = Track(events, lastTick + tickRate.value.toLong) // pad with extra second
+      sequence = Sequence(Vector(track))
+    } yield sequence
+  }
+
+  def writeMidiToFile(sequence: Sequence, path: String): Unit =
+    sequence.write(Paths.get(path).toAbsolutePath.toString)
 }
--- src/test/scala/midi/MidiSpec.scala
+++ src/test/scala/midi/MidiSpec.scala
@@ -1,9 +1,9 @@
 package midi
 
-import de.sciss.midi.{Event, NoteOff, NoteOn}
+import de.sciss.midi.{Event, NoteOff, NoteOn, Sequence}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-import midi.Midi.{groupNotes, toNotesWithRests, readMidiNotes}
+import midi.Midi.{createMidiSequence, filterNoteEvents, groupNotes, midiEventsFrom, readMidiNotes, toNotesWithRests}
 import model.{Melody, Note, NoteLength, NoteName, NoteOrRest, Rest}
 import NoteName._
 import NoteLength._
@@ -147,8 +147,52 @@ class MidiSpec extends AnyFlatSpec with Matchers {
     notes shouldBe Right(Melody(orientalInternal))
   }
 
-  it should "handle notes with rests" in {
-    val notes = readMidiNotes(getClass.getResource("fifth.mid").getPath)
+  "readMidiNotes" should "handle notes with rests" in {
+    val notes = readMidiNotes(getClass.getResource("no5.mid").getPath)
     notes shouldBe Right(Melody(fifthInternal))
+  }
+
+  "midiEventsFrom" should "create a list of midi events from list of notes and rests" in {
+    val midiEvents = midiEventsFrom(orientalInternal)
+    midiEvents shouldBe 'right
+    // let's ignore order, but make sure that they start with the same event
+    midiEvents.right.get.headOption shouldBe orientalMidi.headOption
+    midiEvents.right.get.size shouldBe orientalMidi.size
+    midiEvents.right.get should contain theSameElementsAs orientalMidi
+  }
+
+  it should "handle rests" in {
+    val midiEvents = midiEventsFrom(fifthInternal)
+    midiEvents shouldBe 'right
+    midiEvents.right.get.headOption shouldBe fifthMidi.headOption
+    midiEvents.right.get.size shouldBe fifthMidi.size
+    midiEvents.right.get should contain theSameElementsAs fifthMidi
+  }
+
+  it should "propagate error message if MIDI note out of 127-bit range" in {
+    val midiEvents = midiEventsFrom(List(Note(GSharp, 8, Quarter)))
+    midiEvents shouldBe Left("Pitch out of range: 128")
+  }
+
+  "readMidiNotes" should "quantize input sequence" in {
+    val eitherInputMelody = readMidiNotes(getClass.getResource("nokia.mid").getPath)
+    eitherInputMelody shouldBe 'right
+    val inputMelody = eitherInputMelody.right.get
+    val midiSequence = createMidiSequence(inputMelody.sequence)
+    midiSequence.map(seq => filterNoteEvents(seq.tracks(0).events)) shouldBe Right(List(
+      Event(48, NoteOn(0, 88, 100)), Event(72, NoteOff(0, 88, 0)),
+      Event(72, NoteOn(0, 86, 100)), Event(96, NoteOff(0, 86, 0)),
+      Event(96, NoteOn(0, 78, 100)), Event(144, NoteOff(0, 78, 0)),
+      Event(144, NoteOn(0, 80, 100)), Event(192, NoteOff(0, 80, 0)),
+      Event(192, NoteOn(0, 85, 100)), Event(216, NoteOff(0, 85, 0)),
+      Event(216, NoteOn(0, 83, 100)), Event(240, NoteOff(0, 83, 0)),
+      Event(240, NoteOn(0, 74, 100)), Event(288, NoteOff(0, 74, 0)),
+      Event(288, NoteOn(0, 76, 100)), Event(336, NoteOff(0, 76, 0)),
+      Event(336, NoteOn(0, 83, 100)), Event(360, NoteOff(0, 83, 0)),
+      Event(360, NoteOn(0, 81, 100)), Event(384, NoteOff(0, 81, 0)),
+      Event(384, NoteOn(0, 73, 100)), Event(432, NoteOff(0, 73, 0)),
+      Event(432, NoteOn(0, 76, 100)), Event(480, NoteOff(0, 76, 0)),
+      Event(480, NoteOn(0, 81, 100)), Event(672, NoteOff(0, 81, 0))
+    ))
   }
 }
